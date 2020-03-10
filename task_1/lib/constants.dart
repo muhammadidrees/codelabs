39b7221e22eeea8a85242565a38abc7ce4941cf1
MuhammadIdees <MuhammadIdees@users.noreply.github.com>
2020-03-10 11:43:32 +0500

    created consts file and added colors to theme
    also added assets and updated yaml file

---
+++ task_1/lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+enum status{
+  pending, 
+  wroking, 
+  resolved,
+}
+
+Color pending_status = Color(0xFFF1592A);
+Color working_status = Color(0xFFFCB040);
+Color resolved_status = Color(0xFFE7E7E9);
